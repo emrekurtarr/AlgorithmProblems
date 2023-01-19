e98cac79b2a6c262fee96c6be3437fc8e12489aa
Emre KURTAR <emrekurtar007@gmail.com>
2023-01-19 23:42:48 +0300

    BestTimetoBuyAndSellStock problem solved

--- BestTimeToBuyAndSellStock/Program.cs
+++ BestTimeToBuyAndSellStock/Program.cs
@@ -5,40 +5,24 @@
 int MaxProfit(int[] prices)
 {
 
-    while ()
+    int leftPointer = 0;
+    int rightPointer = 1;
+    int maxProfit = 0;
+
+    while(rightPointer < prices.Length)
     {
-        int firstTimeMinValue = prices.Min();
-        int firstTimeMaxValue = prices.Max();
-
-        int maxProfitFirstMethod = 0;
-        int maxProfitSecondMethod = 0;
-
-        if (Array.IndexOf(prices, firstTimeMinValue) > Array.IndexOf(prices, firstTimeMaxValue))
+        if (prices[leftPointer] > prices[rightPointer])
         {
-            while(prices.Length > 1)
-            {
-                int minValue = prices.Min();
-                int maxValue = prices.Max();
-                if (Array.IndexOf(prices, minValue) > Array.IndexOf(prices, maxValue))
-                {
-                    prices = prices.Skip(Array.IndexOf(prices, maxValue)).ToArray();
-                    continue;
-                }
-                maxProfitFirstMethod = maxValue - minValue;    
-            }
-            
-        }
-        else(Array.IndexOf(prices, firstTimeMinValue) < Array.IndexOf(prices, firstTimeMaxValue))
-        {
-            while(prices.Length )
+            leftPointer = rightPointer;
+            rightPointer++;
+            continue;
         }
 
-
+        maxProfit = Math.Max(maxProfit, prices[rightPointer] - prices[leftPointer]);
+        rightPointer++;
     }
-    
-    
 
-
+    return maxProfit;
 }
 
 
