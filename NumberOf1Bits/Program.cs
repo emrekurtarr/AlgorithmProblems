7e4e6e422e165b151873bc139e03ff48386f712a
Emre KURTAR <emrekurtar007@gmail.com>
2023-03-05 19:05:24 +0300

    NumberOf1Bits added

---
+++ NumberOf1Bits/Program.cs
@@ -0,0 +1,25 @@
+﻿
+
+using System.Diagnostics.Metrics;
+using System.Runtime.CompilerServices;
+
+int HammingWeight(uint n)
+{
+    int onesCounter = 0;
+    
+    while( n > 0)
+    {
+        uint myNum = n & 1;
+
+        if (myNum == 1)
+            onesCounter++;
+
+        n >>= 1;
+    }
+
+    return onesCounter;
+}
+
+uint x = 1011u;
+ 
+Console.WriteLine(HammingWeight(x));
