7b8560def504bccf99e8ddfec8639083aab13aa5
Emre KURTAR <emrekurtar007@gmail.com>
2023-03-28 08:20:50 +0300

    Climbing Stairs solved

---
+++ 70.ClimbingStairs/Program.cs
@@ -0,0 +1,26 @@
+﻿
+
+
+
+Console.WriteLine(ClimbStairs(5));
+
+
+
+
+
+int ClimbStairs(int n)
+{
+	int[] array = new int[n + 1];
+
+	array[0] = 1;
+	array[1] = 1;
+
+	for (int i = 2; i <= n; i++)
+	{
+		array[i] += array[i - 1] + array[i - 2];
+	}
+
+	return array[n];
+
+
+}
