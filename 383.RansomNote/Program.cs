745956e3101875261a7829521f12dba6f7f3e6c7
Emre KURTAR <emrekurtar007@gmail.com>
2023-03-28 07:49:04 +0300

    RansomNote solved

---
+++ 383.RansomNote/Program.cs
@@ -0,0 +1,52 @@
+﻿
+
+
+Console.WriteLine(CanConstruct("aa", "aab"));
+
+
+bool CanConstruct(string ransomNote, string magazine)
+{
+    //List<char> charList = new List<char>();
+
+    //foreach (char item in magazine)
+    //{
+    //    charList.Add(item);
+    //}
+
+    //foreach (char item in ransomNote)
+    //{
+    //    if (!charList.Where(x => x.Equals(item)).Any())
+    //        return false;
+    //    else
+    //        charList.Remove(item);
+
+    //}
+    //magazine.Remove()
+    //return true;
+
+    ////////////////////////////////////////////////////////////////////////////
+
+
+    // Faster way
+
+    int[] array = new int[26];
+
+    foreach (char item in magazine)
+    {
+        array[item - 'a']++;
+    }
+
+    foreach (char item in ransomNote)
+    {
+        array[item - 'a']--;
+    }
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < 0)
+            return false;
+    }
+
+    return true;
+
+}
