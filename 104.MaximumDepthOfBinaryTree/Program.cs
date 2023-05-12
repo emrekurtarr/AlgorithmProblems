dda1665e01abbf3c7fcd7f653da9c7500c9da2e6
Emre KURTAR <emrekurtar007@gmail.com>
2023-05-12 11:59:10 +0300

    104.MaximumDepthOfBinaryTree solved

---
+++ 104.MaximumDepthOfBinaryTree/Program.cs
@@ -0,0 +1,37 @@
+﻿
+
+TreeNode rootNode = new TreeNode(0);
+//rootNode.right = new TreeNode(20, new TreeNode(15, null, null), new TreeNode(7, null, null));
+//rootNode.left = new TreeNode(9, null, null);
+
+Console.WriteLine(MaxDepth(rootNode));
+
+int MaxDepth(TreeNode root)
+{
+
+    if (root == null)
+        return 0;
+    if (root.right == null && root.left == null)
+        return 1;
+
+    int leftDepth = MaxDepth(root.left);
+    int rightDepth = MaxDepth(root.right);
+    return Math.Max(leftDepth, rightDepth) + 1;
+
+}
+
+
+
+
+public class TreeNode
+{
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val = 0, TreeNode left = null, TreeNode right = null)
+    {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
---
+++ TestForTest/Program.cs
@@ -0,0 +1,30 @@
+﻿
+
+int maxNetwork(List<int> speed,int minComps,long speedThres)
+{
+
+    int addedSpeed = 0;
+    int maxNetworkCount = 0;
+    int speedAmount = Convert.ToInt32(speedThres);
+
+    for (int i = 0; i < speed.Count; i++)
+    {
+        addedSpeed += speed[i];
+
+        if (addedSpeed >= speedAmount)
+        {
+            maxNetworkCount++;
+            addedSpeed = 0;
+        }
+    }
+
+    return maxNetworkCount;
+}
+
+OutMethod(out int a);
+
+Console.WriteLine(a);
+static void OutMethod(out int x)
+{
+    x = 100;
+}
